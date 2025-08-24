3314e9318046c7447b54ac47c5df7ce0409fb3ca
Okpainmo <okpainmoandrew@gmail.com>
2025-08-24 23:31:55 +0100

    chore: token base contract rename

--- contracts/Base__LolaUSD.sol
+++ contracts/Base__LolaUSD.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "./interfaces/Interface__AdminManagement.sol";
-import "./interfaces/Interface__ProposalManagement.sol";
+import "./interfaces/IAdminManagement__Base.sol";
+import "./interfaces/IProposalManagement__Base.sol";
 
-contract LolaUSD {
+contract Base__LolaUSD {
     error LolaUSD__InsufficientBalance();
     error LolaUSD__OperatorAddressIsZeroAddress();
     error LolaUSD__OwnerAddressIsZeroAddress();
@@ -17,70 +17,86 @@ contract LolaUSD {
     error LolaUSD__ProposalAlreadyExecuted();
     error LolaUSD__InvalidProposalCodeName();
 
-    event Transfer(address indexed _owner, address indexed _receiver, uint256 _value);
-    event Approval(address indexed _owner, address indexed _operator, uint256 _value);
+    event Transfer(
+        address indexed _owner,
+        address indexed _receiver,
+        uint256 _value
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _operator,
+        uint256 _value
+    );
 
-    address internal s_adminManagementCoreContractAddress;// needed to check admin rights and likely more
+    address internal s_adminManagementCoreContractAddress; // needed to check admin rights and likely more
     address internal s_proposalManagementCoreContractAddress; // needed for interaction with the external proposals management core contract
 
-    IAdminManagement internal adminMangementContract = IAdminManagement(s_adminManagementCoreContractAddress);
-    IProposalManagement internal proposalManagementContract = IProposalManagement(s_proposalManagementCoreContractAddress);
+    IAdminManagement__Base internal s_adminMangementContract =
+        IAdminManagement__Base(s_adminManagementCoreContractAddress);
+    IProposalManagement__Base internal s_proposalManagementContract =
+        IProposalManagement__Base(s_proposalManagementCoreContractAddress);
 
-    string internal tokenName;
-    string internal tokenSymbol;
-    uint8 internal tokenDecimals;
-    address internal owner;
-    uint256 internal supply;
+    string internal s_tokenName;
+    string internal s_tokenSymbol;
+    uint8 internal s_tokenDecimals;
+    uint256 internal s_supply;
 
     mapping(address => uint256) internal balance;
     mapping(address => mapping(address => uint256)) internal allowedSpend;
-    
+
     function name() public view returns (string memory) {
-        return tokenName;
+        return s_tokenName;
     }
 
     function symbol() public view returns (string memory) {
-        return tokenSymbol;
+        return s_tokenSymbol;
     }
 
     function decimals() public view returns (uint8) {
-        return tokenDecimals;
+        return s_tokenDecimals;
     }
 
     function totalSupply() public view returns (uint256) {
-        return supply / (10 ** tokenDecimals);
+        return s_supply / (10 ** s_tokenDecimals);
     }
 
-    function balanceOf(address _owner) public view returns (uint256){
-        return balance[_owner] / (10 ** tokenDecimals);
-    }    
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balance[_owner] / (10 ** s_tokenDecimals);
+    }
 
     function approve(address _operator, uint256 _value) public returns (bool) {
-        _value = _value * 10 ** tokenDecimals;
+        _value = _value * 10 ** s_tokenDecimals;
 
-        if(_operator == address(0)) {
+        if (_operator == address(0)) {
             revert LolaUSD__OperatorAddressIsZeroAddress();
         }
 
         allowedSpend[msg.sender][_operator] = _value; // user(msg.sender) allows platform/operator, e.g. a DEX, to currently be able to spend '_value' amount from their balance
 
         emit Approval(msg.sender, _operator, _value);
-        
+
         return true;
     }
 
-    function allowance(address _owner, address _operator) public view returns (uint256) {
-        return allowedSpend[_owner][_operator] / (10 ** tokenDecimals); // check how much a user has allowed an operator to spend
+    function allowance(
+        address _owner,
+        address _operator
+    ) public view returns (uint256) {
+        return allowedSpend[_owner][_operator] / (10 ** s_tokenDecimals); // check how much a user has allowed an operator to spend
     }
 
-    function increaseAllowance(address _operator, uint256 _amountToAdd) external returns (bool) {
-        _amountToAdd = _amountToAdd * 10 ** tokenDecimals;
+    function increaseAllowance(
+        address _operator,
+        uint256 _amountToAdd
+    ) external returns (bool) {
+        _amountToAdd = _amountToAdd * 10 ** s_tokenDecimals;
 
         if (_operator == address(0)) {
             revert LolaUSD__OperatorAddressIsZeroAddress();
         }
-        
-        uint256 newAllowance = allowedSpend[msg.sender][_operator] + _amountToAdd;
+
+        uint256 newAllowance = allowedSpend[msg.sender][_operator] +
+            _amountToAdd;
 
         allowedSpend[msg.sender][_operator] = newAllowance;
 
@@ -89,18 +105,21 @@ contract LolaUSD {
         return true;
     }
 
-    function decreaseAllowance(address _operator, uint256 _amountToDeduct) external returns (bool) {
-        _amountToDeduct = _amountToDeduct * 10 ** tokenDecimals;
+    function decreaseAllowance(
+        address _operator,
+        uint256 _amountToDeduct
+    ) external returns (bool) {
+        _amountToDeduct = _amountToDeduct * 10 ** s_tokenDecimals;
 
         if (_operator == address(0)) {
             revert LolaUSD__OperatorAddressIsZeroAddress();
-        } 
+        }
 
         uint256 currentAllowance = allowedSpend[msg.sender][_operator];
 
         if (_amountToDeduct > currentAllowance) {
             revert LolaUSD__ExcessAllowanceDecrement();
-        } 
+        }
 
         uint256 newAllowance = currentAllowance - (_amountToDeduct);
 
@@ -110,15 +129,15 @@ contract LolaUSD {
 
         return true;
     }
-    
-    function transfer(address _receiver, uint256 _value) public returns (bool) {
-        _value = _value * 10 ** tokenDecimals;
 
-        if(_receiver == address(0)) {
+    function transfer(address _receiver, uint256 _value) public returns (bool) {
+        _value = _value * 10 ** s_tokenDecimals;
+
+        if (_receiver == address(0)) {
             revert LolaUSD__ReceiverAddressIsZeroAddress();
         }
 
-        if(_value > balance[msg.sender]) {
+        if (_value > balance[msg.sender]) {
             revert LolaUSD__InsufficientBalance();
         }
 
@@ -130,8 +149,12 @@ contract LolaUSD {
         return true;
     }
 
-    function transferFrom(address _owner, address _receiver, uint256 _value) public returns (bool) {
-        _value = _value * 10 ** tokenDecimals;
+    function transferFrom(
+        address _owner,
+        address _receiver,
+        uint256 _value
+    ) public returns (bool) {
+        _value = _value * 10 ** s_tokenDecimals;
 
         if (_receiver == address(0)) {
             revert LolaUSD__ReceiverAddressIsZeroAddress();
@@ -159,7 +182,7 @@ contract LolaUSD {
         //     allowedSpend[_owner][msg.sender] = currentAllowance - _value;
         //     emit Approval(_owner, msg.sender, allowedSpend[_owner][msg.sender]); // optional but good practice
         // }
-        
+
         allowedSpend[_owner][msg.sender] = currentAllowance - _value;
         emit Approval(_owner, msg.sender, allowedSpend[_owner][msg.sender]); // optional but good practice
         // allowedSpend[_owner][msg.sender] =  0;
@@ -169,51 +192,79 @@ contract LolaUSD {
         return true;
     }
 
-    function mint(address _to, uint256 _amount, uint256 _proposalId, string memory _proposalCodeName) public  {
-        _amount = _amount * 10 ** tokenDecimals;
+    function mint(
+        address _to,
+        uint256 _amount,
+        uint256 _proposalId,
+        string memory _proposalCodeName
+    ) public {
+        _amount = _amount * 10 ** s_tokenDecimals;
 
-        if(!adminMangementContract.checkIsAdmin(msg.sender)) {
+        if (!s_adminMangementContract.checkIsAdmin(msg.sender)) {
             revert LolaUSD__AccessDenied_AdminOnly();
         }
 
-        IProposalManagement.Proposal memory proposal = proposalManagementContract.getProposalById(_proposalId);
+        IProposalManagement__Base.Proposal
+            memory proposal = s_proposalManagementContract.getProposalById(
+                _proposalId
+            );
 
-        if (proposal.proposalStatus == IProposalManagement.ProposalStatus.Executed) {
+        if (
+            proposal.proposalStatus ==
+            IProposalManagement__Base.ProposalStatus.Executed
+        ) {
             revert LolaUSD__ProposalAlreadyExecuted();
         }
 
-        if (keccak256(bytes(proposal.proposalCodeName)) != keccak256(bytes(_proposalCodeName))) {
+        if (
+            keccak256(bytes(proposal.proposalCodeName)) !=
+            keccak256(bytes(_proposalCodeName))
+        ) {
             revert LolaUSD__InvalidProposalCodeName();
         }
 
         if (_to == address(0)) {
             revert LolaUSD__ReceiverAddressIsZeroAddress();
-        } 
+        }
 
-        supply += _amount;
+        s_supply += _amount;
 
         balance[_to] += _amount;
 
         emit Transfer(address(0), _to, _amount);
 
         // updates the status of the proposal in the external proposal-management core contract to "executed"
-        proposalManagementContract.executeProposal(_proposalId);
+        s_proposalManagementContract.executeProposal(_proposalId);
     }
 
-    function burn(address _from, uint256 _amount, uint256 _proposalId, string memory _proposalCodeName) public {
-        _amount = _amount * 10 ** tokenDecimals;
+    function burn(
+        address _from,
+        uint256 _amount,
+        uint256 _proposalId,
+        string memory _proposalCodeName
+    ) public {
+        _amount = _amount * 10 ** s_tokenDecimals;
 
-        if(!adminMangementContract.checkIsAdmin(msg.sender)) {
+        if (!s_adminMangementContract.checkIsAdmin(msg.sender)) {
             revert LolaUSD__AccessDenied_AdminOnly();
         }
 
-        IProposalManagement.Proposal memory proposal = proposalManagementContract.getProposalById(_proposalId);
+        IProposalManagement__Base.Proposal
+            memory proposal = s_proposalManagementContract.getProposalById(
+                _proposalId
+            );
 
-        if (proposal.proposalStatus == IProposalManagement.ProposalStatus.Executed) {
+        if (
+            proposal.proposalStatus ==
+            IProposalManagement__Base.ProposalStatus.Executed
+        ) {
             revert LolaUSD__ProposalAlreadyExecuted();
         }
 
-        if (keccak256(bytes(proposal.proposalCodeName)) != keccak256(bytes(_proposalCodeName))) {
+        if (
+            keccak256(bytes(proposal.proposalCodeName)) !=
+            keccak256(bytes(_proposalCodeName))
+        ) {
             revert LolaUSD__InvalidProposalCodeName();
         }
 
@@ -225,12 +276,14 @@ contract LolaUSD {
             revert LolaUSD__InsufficientBalance();
         }
 
-        if (_from == msg.sender) { // if self(master or admin) address, proceed to burn directly
+        if (_from == msg.sender) {
+            // if self(master or admin) address, proceed to burn directly
             balance[_from] -= _amount;
-            supply -= _amount;
+            s_supply -= _amount;
 
             emit Transfer(msg.sender, address(0), _amount);
-        } else { // if not self address, proceed to burn via allowance 
+        } else {
+            // if not self address, proceed to burn via allowance
             uint256 currentAllowance = allowedSpend[_from][msg.sender];
 
             if (currentAllowance < _amount) {
@@ -238,15 +291,15 @@ contract LolaUSD {
             }
 
             balance[_from] -= _amount;
-            supply -= _amount;
+            s_supply -= _amount;
 
             allowedSpend[_from][msg.sender] = currentAllowance - _amount;
             emit Approval(_from, msg.sender, allowedSpend[_from][msg.sender]);
 
             emit Transfer(_from, address(0), _amount);
-        }        
+        }
 
         // updates the status of the proposal in the external proposal-management core contract to "executed"
-        proposalManagementContract.executeProposal(_proposalId);
+        s_proposalManagementContract.executeProposal(_proposalId);
     }
 }
